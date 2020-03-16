abe6df24bf336bb0dd78d49d551a5a9742306ea1
Dencan <dencan.gan@gmail.com>
2020-03-16 22:05:04 +0000

    Added structs.go for struct practice

--- jsonHandling.go
+++ jsonHandling.go
@@ -10,56 +10,26 @@ import (
 	//"os"
 )
 
-type response1 struct {
-	Page int
-	Fruits []string
-}
 
-// Tags on struct field to customize endoded JSON key names
-type response2 struct {
-	Page int `json: "page"`
-	Fruits []string `json: "fruits"`
-}
-
-func jsonEncoder(inputDatatype string) {
+/*
+Encodes data types into JSON format
+	Bool: true
+	Int: 1
+	Float: 2.34
+	String: "gopher"
+	Array: []string{"apple", "peach", "pear"}
+	Maps: map[string]int{"apple": 5, "lettuce": 7}
+*/
+func jsonEncoder(inputDatatype interface{}) {
 	
 	output, _ := json.Marshal(inputDatatype)
 	
 	// Preview
 	fmt.Println(string(output))
+	return
 }
 
 func main() {
 
-	// Encoding basic data types to JSON strings
-	bolB, _:= json.Marshal(true)
-	fmt.Println(string(bolB))
-	intB, _ := json.Marshal(1)
-    fmt.Println(string(intB))
-    fltB, _ := json.Marshal(2.34)
-	fmt.Println(string(fltB))
-	strB, _ := json.Marshal("gopher")
-	fmt.Println(string(strB))
-
-	// Array	
-	slcD := []string{"apple", "peach", "pear"}
-	slcB, _ := json.Marshal(slcD)
-	fmt.Println(string(slcB))
-
-	// Maps
-	mapD := map[string]int{"apple": 5, "lettuce": 7}
-	mapB, _ := json.Marshal(mapD)
-	fmt.Println(string(mapB))
-
-	res1D := &response1{
-        Page:   1,
-        Fruits: []string{"apple", "peach", "pear"}}
-    res1B, _ := json.Marshal(res1D)
-    fmt.Println(string(res1B))
-
-	res2D := &response2{
-        Page:   1,
-        Fruits: []string{"apple", "peach", "pear"}}
-    res2B, _ := json.Marshal(res2D)
-	fmt.Println(string(res2B))
-	}
+	jsonEncoder([]string{"apple", "peach", "pear"})
+}
---
+++ pointers.go
@@ -0,0 +1,7 @@
+/*
+& operator gets the variable's memory address.
+* operator gets the variable's value.
+*/
+
+package main
+
